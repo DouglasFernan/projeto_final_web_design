ffe0ee06472fe685e5c91d9a5e259fec123a1c5f
Douglas Fernandes <douglasgenetic@gmail.com>
2024-08-31 13:20:11 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const pointers = document.querySelectorAll(".pointer");
+
+function mudarSlide(index) {
+    currentIndex = index;
+    const marginLeft = `${currentIndex * -100}%`;
+    document.querySelector(".sliders").style.marginLeft = marginLeft;
+    updatePointers();
+}
+
+function updatePointers() {
+    pointers.forEach((pointer, index) => {
+        pointer.style.backgroundColor =
+            index === currentIndex ? "yellow" : "black";
+    });
+}
+
+function startCarousel() {
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % slides.length;
+        const marginLeft = `${currentIndex * -100}%`;
+        document.querySelector(".sliders").style.marginLeft = marginLeft;
+        updatePointers();
+    }, 3000); // 3 segundos (3000 milissegundos) de intervalo entre cada transição
+}
+
+startCarousel();
